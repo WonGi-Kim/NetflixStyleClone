28d45ef72934e448504291829de84fdc67dd8437
WonGi-Kim <kwg970310@gmail.com>
2023-03-01 16:56:01 +0900

    Initial Commit

---
+++ NetflixStyleClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixStyleClone
+//
+//  Created by 김원기 on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
